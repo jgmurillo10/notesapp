8db84c928d6d6ed8a36306814ca586c0e2bc4511
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2023-08-23 09:42:57 -0500

    Fix dark mode eyebrow in featured

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -20,7 +20,7 @@ export const Featured = async ({ lang }: { lang: Locale }) => {
     <div className="bg-white dark:bg-black py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600 dark:text-indigo-300">
             {dictionary['featured'].eyebrow}
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
