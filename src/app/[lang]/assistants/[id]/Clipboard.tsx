10e83f9f7d667a8d8c8363261f31aed3a119b35c
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2023-08-25 15:53:07 -0500

    remove alert and show copied

--- src/app/[lang]/assistants/[id]/Clipboard.tsx
+++ src/app/[lang]/assistants/[id]/Clipboard.tsx
@@ -1,11 +1,13 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
 
 export const Clipboard = ({ children }: { children: React.ReactNode }) => {
+  const [isCopied, setIsCopied] = useState(false);
   const handleCopyToClipboard = async () => {
     const currentUrl = window.location.href;
     await navigator.clipboard.writeText(currentUrl);
-    alert(`Copied '${currentUrl}' to clipboard!`);
+    setIsCopied(true);
   };
 
   return (
@@ -14,7 +16,14 @@ export const Clipboard = ({ children }: { children: React.ReactNode }) => {
       type="button"
       className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
     >
-      {children}
+      {isCopied ? (
+        <div className="flex items-center">
+          <CheckCircleIcon className="h-6 w-6 mr-2 text-green-400" />
+          Copied
+        </div>
+      ) : (
+        children
+      )}
     </button>
   );
 };
