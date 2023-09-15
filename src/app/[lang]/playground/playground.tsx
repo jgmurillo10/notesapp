7c923f71d852e1b978a77218cf7a5f3a7052ddfb
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2023-09-15 17:53:47 -0500

    Update playground styles

--- src/app/[lang]/playground/playground.tsx
+++ src/app/[lang]/playground/playground.tsx
@@ -8,8 +8,10 @@
 import { FormEvent, useState } from 'react';
 import type { Message } from 'ai/react';
 import { v4 as uuidv4 } from 'uuid';
+import { MinusCircleIcon } from '@heroicons/react/24/outline';
+import { Editable } from '@/components/editable/editable';
 
-type LocalMessage = Pick<Message, 'role' | 'content'> & { uuid: string };
+export type LocalMessage = Pick<Message, 'role' | 'content'> & { uuid: string };
 
 const emptyMessageGenerator = (): LocalMessage => ({
   uuid: uuidv4(),
@@ -19,6 +21,7 @@ const emptyMessageGenerator = (): LocalMessage => ({
 
 export const Playground = () => {
   const [systemMessage, setSystemMessage] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [messages, setMessages] = useState<LocalMessage[]>([
     emptyMessageGenerator(),
   ]);
@@ -44,6 +47,7 @@ export const Playground = () => {
     while (true) {
       const { value, done } = await reader.read();
       if (done) {
+        setIsLoading(false);
         break;
       }
 
@@ -78,6 +82,12 @@ export const Playground = () => {
     );
   };
 
+  const handleRemove = (id: string) => {
+    setMessages((newMessages) =>
+      newMessages.filter((message) => message.uuid !== id),
+    );
+  };
+
   const toggleRole = (id: string) => {
     setMessages((newMessages) =>
       newMessages.map((message) =>
@@ -103,6 +113,7 @@ export const Playground = () => {
 
     const submitMessages: LocalMessage[] = [system, ...messages];
 
+    setIsLoading(true);
     const response = await fetch('/api/chat', {
       method: 'POST',
       body: JSON.stringify({
@@ -116,14 +127,14 @@ export const Playground = () => {
   };
 
   return (
-    <div className="flex">
+    <div className="flex gap-4 mt-8">
       <div className="flex-2">
         <h3>System</h3>
         <textarea
           value={systemMessage}
           onChange={(e) => setSystemMessage(e.target.value)}
           placeholder="You are a helpful assistant"
-          className="w-full max-w-lg bottom-0 border border-gray-300 focus:ring-4 focus:ring-blue-300 rounded-l text-black"
+          className="w-full mt-8 max-w-lg bottom-0 border border-gray-300 focus:ring-4 focus:ring-blue-300 rounded-l text-black h-96"
         />
       </div>
       <div className="flex-1">
@@ -131,27 +142,42 @@ export const Playground = () => {
         <form onSubmit={handleSubmit}>
           <ul>
             {messages.map((message) => (
-              <li key={message.uuid}>
-                <button type="button" onClick={() => toggleRole(message.uuid)}>
+              <li
+                key={message.uuid}
+                className="flex border-b border-slate-300 dark:border-gray-200 py-8 gap-2"
+              >
+                <button
+                  disabled={isLoading}
+                  className="flex w-32 h-full justify-center items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-900 focus:outline-none dark:focus:ring-blue-800 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                  type="button"
+                  onClick={() => toggleRole(message.uuid)}
+                >
                   {message.role}
                 </button>
-                <textarea
-                  className="text-black"
-                  onChange={(e) => handleOnChange(e, message.uuid)}
-                  value={message.content}
-                  placeholder={`Enter an ${message.role} message here.`}
-                />
+                <Editable message={message} handleOnChange={handleOnChange} />
+                <button
+                  disabled={isLoading}
+                  onClick={() => handleRemove(message.uuid)}
+                >
+                  <MinusCircleIcon className="h-6 w-6 ml-1" />
+                </button>
               </li>
             ))}
           </ul>
-          <button type="button" onClick={handleAddMessage}>
+          <button
+            className="mt-8 flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-900 focus:outline-none dark:focus:ring-blue-800 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            type="button"
+            onClick={handleAddMessage}
+          >
             Add message
           </button>
-          <input type="submit" />
+          <input
+            disabled={isLoading}
+            className="my-24 flex items-center justify-center text-white bg-blue-700 w-32 text-center hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-900 focus:outline-none dark:focus:ring-blue-800 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            type="submit"
+            value={isLoading ? 'Loading' : 'Send'}
+          />
         </form>
-      </div>
-      <div className="flex-2">
-        <h3>Setup</h3>
       </div>
     </div>
   );
---
+++ src/components/editable/editable.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { LocalMessage } from '@/app/[lang]/playground/playground';
+import { useEffect, useRef, useState } from 'react';
+
+export const Editable = ({
+  message,
+  handleOnChange,
+}: {
+  message: LocalMessage;
+  handleOnChange: (e: React.SyntheticEvent, id: string) => void;
+}) => {
+  const [isEditable, setIsEditable] = useState<boolean>(false);
+  const inputRef = useRef<HTMLTextAreaElement>();
+  const placeholder = `Enter an ${message.role} message here.`;
+
+  const handleEditMode = () => {
+    setIsEditable(true);
+
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const handleKeyUp = () => {
+    inputRef.current.style.height = '1px';
+    inputRef.current.style.height = inputRef.current.scrollHeight + 'px';
+  };
+
+  return (
+    <>
+      <textarea
+        ref={inputRef}
+        onBlur={() => setIsEditable(false)}
+        className={`text-black h-[40px] w-full ${isEditable ? '' : 'hidden'}`}
+        onChange={(e) => handleOnChange(e, message.uuid)}
+        onKeyUp={handleKeyUp}
+        onFocus={handleKeyUp}
+        value={message.content}
+        placeholder={placeholder}
+      />
+      <div
+        onClick={handleEditMode}
+        className={`p-2 min-h-full whitespace-pre-wrap max-w-full w-full text-black border-2 border-transparent hover:bg-slate-100 hover:cursor-pointer dark:bg-white ${
+          isEditable ? 'hidden' : ''
+        }`}
+      >
+        {message.content ? message.content : placeholder}
+      </div>
+    </>
+  );
+};
